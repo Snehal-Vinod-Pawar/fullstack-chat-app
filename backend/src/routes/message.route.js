da5a9de99e13b538baace83fc2c6f72596ef6a19
Snehal Pawar <snehalpawar2005@gmail.com>
2025-07-22 14:10:39 +0530

    🔥 Fixed message route crash by renaming dynamic routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.get("/users",protectRoute, getUserForSidebar)
 
-router.get("/:id", protectRoute,getMessages)
+router.get("/messages/:id", protectRoute,getMessages)
 
-router.post("/send/:id",protectRoute, sendMessages)
+router.post("/messages/send/:id",protectRoute, sendMessages)
 
 export default router;
