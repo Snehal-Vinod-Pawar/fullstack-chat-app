d69aa2ecb9581e8c925b61db874da18b77750216
Snehal Pawar <snehalpawar2005@gmail.com>
2025-07-22 14:29:52 +0530

    🐛 Fix: Adjusted message routes to avoid route conflicts

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.get("/users",protectRoute, getUserForSidebar)
 
-router.get("/:id", protectRoute,getMessages)
+router.get("/chat/:id", protectRoute,getMessages)
 
-router.post("/send/:id",protectRoute, sendMessages)
+router.post("/chat/send/:id",protectRoute, sendMessages)
 
 export default router;
