88806556fc4f2702bc4d71aeca835a4fadada9e9
Snehal Pawar <snehalpawar2005@gmail.com>
2025-07-24 18:02:33 +0530

    Fix: corrected build script and ready for deployment

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/chat/:id", protectRoute, getMessages);
 
-router.post("/chat/send/:id", protectRoute, sendMessage);
+router.post("/chat/:id/send", protectRoute, sendMessage);
 
 export default router;
