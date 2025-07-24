75314ed992d379799c3194afcbf4c98e73243ec6
Snehal Pawar <snehalpawar2005@gmail.com>
2025-07-24 18:10:43 +0530

    Fix: corrected build script and ready for deployment

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -25,7 +25,7 @@ export const useChatStore = create((set, get) => ({
   getMessages: async (userId) => {
     set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`/messages/chat/${userId}`);
+      const res = await axiosInstance.get(`/messages/${userId}`);
       set({ messages: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
@@ -36,7 +36,7 @@ export const useChatStore = create((set, get) => ({
   sendMessage: async (messageData) => {
     const { selectedUser, messages } = get();
     try {
-      const res = await axiosInstance.post(`/messages/chat/${selectedUser._id}/send`, messageData);
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
       set({ messages: [...messages, res.data] });
     } catch (error) {
       toast.error(error.response.data.message);
